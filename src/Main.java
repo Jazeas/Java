3d50a32626933fef0b14a98992351f0f148a4587
Jazeas <mssivukhin2@gmail.com>
2026-03-11 09:16:37 +0300

    Список решений задач

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! fafarere");
+        System.out.println("тут будут решения задач чтобы не забывать");
     }
 }

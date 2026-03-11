abdecd9a6cf5ae59b991ba52f9619a4f6eadd2a3
Jazeas <mssivukhin2@gmail.com>
2026-03-11 20:06:27 +0300

    Инкапсуляция-Задача 1. Кнопка лифта Создайте класс ElevatorButton. Скрытое поле targetFloor (целевой этаж). Сделайте геттер и сеттер с проверкой: этаж не может быть отрицательным и больше 20.

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,29 @@
 public class Main {
-    private double balance;
-    private double hold;
+    public static class ElevatorButton{
+        private int targetFloor;
 
-    public Main(double initialBalance) {
-        this.balance = initialBalance;
-        this.hold = 0;
+        public int getTargetFloor(){
+            System.out.println(targetFloor);
+            return targetFloor;
+        }
 
-    }
-
-    public void hold(double amount){
-        AssertAmountIsPositive(amount);
-        AssertBalanceIsSufficient(amount);
-
-        balance -= amount;
-        hold += amount;
-
-        System.out.println("✓ Заморожено: " + amount);
-        System.out.println("  Доступно: " + balance);
-        System.out.println("  Заморожено всего: " + hold);
-        System.out.println("--------------------");
-    }
-    private void AssertAmountIsPositive(double amount){
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Сумма должна быть положительной: " + amount);
+        public void setTargetFloor(int targetFloor) {
+            if(targetFloor >=0 && targetFloor <= 20){
+                System.out.println("Вы успешно поднялись на этаж: " + targetFloor);
+                this.targetFloor = targetFloor;
+            }else{
+                System.out.println("Ошибка! этажи в диапазоне от 0 - 20, а указанный этаж: " + targetFloor);
+            }
         }
     }
-    private void AssertBalanceIsSufficient(double amount){
-        if (amount > balance) {
-            throw new IllegalArgumentException("Недостаточно средств. Доступно: " + balance + ", запрошено: " + amount);
-        }
-    }
-
-    public double getBalance(){return balance;}
-    public double getHold(){return hold;}
-
     public static void main(String[] args){
+        ElevatorButton button = new ElevatorButton();
 
-       Main account = new Main(1000);
+        button.setTargetFloor(-12);
 
-        System.out.println("=== Начальное состояние ===");
-        System.out.println("Баланс: " + account.getBalance());
-        System.out.println("Заморожено: " + account.getHold());
-        System.out.println("====================\n");
+        button.getTargetFloor();
 
-        account.hold(200);
-
-        account.hold(200);
-
-        try {
-            account.hold(700);
-        } catch ( IllegalArgumentException e ) {
-            System.out.println("❌ Ошибка: " + e.getMessage());
-        }
-
-        System.out.println("\n=== Итоговое состояние ===");
-        System.out.println("Баланс: " + account.getBalance());
-        System.out.println("Заморожено: " + account.getHold());
+        button.setTargetFloor(13);
 
     }
 

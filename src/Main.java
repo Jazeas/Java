1d0aaeb8ff5dcc79583716f7538494cc70dc9a31
Jazeas <mssivukhin2@gmail.com>
2026-03-12 21:53:14 +0300

    Наследование-Задача 2. Сотрудники Класс Employee (поля: name, baseSalary). Метод calculateSalary() возвращает baseSalary. Наследники:
    Manager — добавляет бонус, метод переопределить: baseSalary + 20000
    
    Intern — зарплата = baseSalary / 2
    Вызовите метод для всех в массиве. обьясни больше задачу

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,53 @@
 public class Main {
-    public static class Vehicle{
-        String brand;
-        int speed;
-    }
+    public static class Employee {
+        private String name;
+        private int baseSalary;
 
-    public static class Car extends Vehicle{
-        int fuelType;
+        public Employee(String name, int baseSalary){
+            this.name = name;
+            this.baseSalary = baseSalary;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getBaseSalary() {
+            return baseSalary;
+        }
+
+        public int calculateSalary(){
+            return baseSalary;
+        }
     }
-    public static class Bicycle extends Vehicle {
-        boolean hasBell;
+    public static class Manager extends Employee {
+        public Manager(String name, int baseSalary){
+            super(name,baseSalary);
+        }
+
+        @Override
+        public int calculateSalary(){
+            return super.calculateSalary() + 20000;
+        }
+    }
+    public static class Intern extends Employee {
+        public Intern(String name, int baseSalary){
+            super(name,baseSalary);
+        }
+
+        @Override
+        public int calculateSalary(){
+            return super.calculateSalary() / 2;
+        }
     }
     public static void main(String[] args){
-        Car car = new Car();
-        car.brand = "toyota";
-        car.speed = 130;
-        car.fuelType = 95;
-
-        Bicycle bicycle = new Bicycle();
-        bicycle.brand = "Stels";
-        bicycle.speed = 30;
-        bicycle.hasBell = true;
-
-        System.out.println(car.brand + " " + car.speed + " " + car.fuelType);
-
-        System.out.println(bicycle.brand + " " + bicycle.speed + " " + bicycle.hasBell);
+            Employee[] employees = {
+                    new Manager("Директор",100000),
+                    new Intern("Стажер Петя", 30000),
+                    new Intern("Стажер Ваня", 50000)
+            };
+            for(Employee emp : employees){
+                System.out.println(emp.getName() + ": " + emp.calculateSalary());
+            }
     }
 }

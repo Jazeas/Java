0a3421b1315e3e5026ea4edeeb89553d29b57981
Jazeas <mssivukhin2@gmail.com>
2026-03-14 12:32:56 +0300

    Создать класс Calculator с перегрузкой метода multiply() для int и double.

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,16 @@
 public class Main {
-    public static abstract class Vehicle{
-        private String brand;
+    public static class Calculator{
+        public int multiply(int a, int b){
+            return a * b;
+        }
+        public double multiply(double a, double b){
+            return a * b;
+        }
+    }
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
 
-        public void setBrand(String brand){
-            this.brand = brand;
-        }
-        public abstract void move();
-    }
-    public static class Car extends Vehicle{
-        @Override
-        public void move(){
-            System.out.println("Машина едет по дороге");
-        }
-    }
-    public static class Bike extends  Vehicle{
-        @Override
-        public void move(){
-            System.out.println("Велосипед крутит педали");
-        }
-    }
-    public static void main(String[] args){
-        Vehicle[] vehicles = {
-                new Car(),
-                new Bike()
-        };
-        for( Vehicle veh : vehicles){
-            veh.move();
-        }
+        System.out.println(calc.multiply(23.6, 45));
+        System.out.println(calc.multiply(5,8));
     }
 }

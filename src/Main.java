e2cc6d54c8a3b399f9789a3504679865687fab5f
Jazeas <mssivukhin2@gmail.com>
2026-03-14 17:48:33 +0300

    Generics-Wildcards-Задача: "Фабрика коробок для животных" Контекст: У вас есть иерархия классов животных:
    java
    class Animal { }
    class Mammal extends Animal { }
    class Dog extends Mammal { }
    class Cat extends Mammal { }
    Что нужно сделать:
    Часть 1. Класс Box<T>
    Создайте обобщенный класс Box<T>, который может хранить один элемент типа T:
    
    Поле private T content
    
    Метод void put(T item) — положить предмет
    
    Метод T get() — взять предмет
    
    Метод boolean isEmpty()
    
    Часть 2. Методы с wildcards
    
    Реализуйте статический метод, который перемещает содержимое из одной коробки в другую:
    
    java
    public static void move(Box<? extends T> src, Box<? super T> dest)
    Этот метод должен:
    
    Взять элемент из src (источник)
    
    Положить его в dest (назначение)
    
    Очистить исходную коробку

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,59 @@
-public class Main {
-    public static class Box<T>{
-        T value;
 
-        void set(T value){
-            this.value = value;
+public class Main {
+    static class Animal { }
+    static class Mammal extends Animal { }
+    static class Dog extends Mammal { }
+    static class Cat extends Mammal { }
+    public static class Box<T>{
+        private T content;
+        public void put(T item){
+            this.content = item;
         }
-        T get(){
-            return value;
+        public T get(){
+            return content;
+        }
+        public boolean isEmpty(){
+            return content == null;
+        }
+    }
+    public static <T> void move(Box<? extends T> src, Box<? super T> dest){
+        if(!src.isEmpty()){
+            T item = src.get();
+
+            dest.put(item);
+
+            src.put(null);
+
+            System.out.println("Перемещено успешно!");
+        }else {
+            System.out.println("Исходная коробка пуста!");
         }
     }
     public static void main(String[] args){
-        Box<String> box = new Box<>();
-        box.set("Hello");
-        String s = box.get();
-        System.out.println(box.get());
+        Dog dog = new Dog();
+        Cat cat = new Cat();
 
-        Box<Integer> box1 = new Box<>();
-        box1.set(10);
-        System.out.println(box1.get());
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Mammal> mammalBox = new Box<>();
+
+        dogBox.put(dog);
+        System.out.println("dogBox пуст? " + dogBox.isEmpty());
+
+        System.out.println("Перемещаем собаку в animalBox");
+        move(dogBox, animalBox);
+
+        System.out.println("dogBox пуст? " + dogBox.isEmpty());
+        System.out.println("animalBox пуст? " + animalBox.isEmpty());
+
+        System.out.println("Перемещение кошки");
+        catBox.put(cat);
+
+        System.out.println("catBox пуст? " + catBox.isEmpty());
+        move(catBox, mammalBox);
+
+        System.out.println("mammalBox пуст? " + mammalBox.isEmpty());
+
     }
 }

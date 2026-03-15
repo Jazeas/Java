1d86dc6594f2dd621a770f52364f9f71e58e3fa4
Jazeas <mssivukhin2@gmail.com>
2026-03-15 18:32:38 +0300

    Списки-Задача 1.1: Создание и заполнение Создайте ArrayList<String>, добавьте 5 названий городов. Выведите:
    Все города через цикл for
    
    Все города через for-each
    
    Все города через Iterator

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 public class Main {
-    static class Animal { }
-    static class Mammal extends Animal { }
-    static class Dog extends Mammal { }
-    static class Cat extends Mammal { }
-    public static class Box<T>{
-        private T content;
-        public void put(T item){
-            this.content = item;
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Москва");
+        list.add("Питер");
+        list.add("Самара");
+        list.add("Таганрок");
+        list.add("Владивосток");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
-        public T get(){
-            return content;
+
+        System.out.println("========");
+
+        for (String item : list) {
+            System.out.println(item);
         }
-        public boolean isEmpty(){
-            return content == null;
+
+        System.out.println("========");
+
+        Iterator<String> it = list.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
         }
-    }
-    public static <T> void move(Box<? extends T> src, Box<? super T> dest){
-        if(!src.isEmpty()){
-            T item = src.get();
 
-            dest.put(item);
-
-            src.put(null);
-
-            System.out.println("Перемещено успешно!");
-        }else {
-            System.out.println("Исходная коробка пуста!");
-        }
-    }
-    public static void main(String[] args){
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-
-        Box<Dog> dogBox = new Box<>();
-        Box<Cat> catBox = new Box<>();
-        Box<Animal> animalBox = new Box<>();
-        Box<Mammal> mammalBox = new Box<>();
-
-        dogBox.put(dog);
-        System.out.println("dogBox пуст? " + dogBox.isEmpty());
-
-        System.out.println("Перемещаем собаку в animalBox");
-        move(dogBox, animalBox);
-
-        System.out.println("dogBox пуст? " + dogBox.isEmpty());
-        System.out.println("animalBox пуст? " + animalBox.isEmpty());
-
-        System.out.println("Перемещение кошки");
-        catBox.put(cat);
-
-        System.out.println("catBox пуст? " + catBox.isEmpty());
-        move(catBox, mammalBox);
-
-        System.out.println("mammalBox пуст? " + mammalBox.isEmpty());
 
     }
 }

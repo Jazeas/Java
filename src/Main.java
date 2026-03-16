8e0c940aa3f8ce6292807c42c0d2fa30732efe58
Jazeas <mssivukhin2@gmail.com>
2026-03-16 20:08:07 +0300

    5. Сортировка строк
    Есть список:
    
    List<String> names = List.of("Alex", "John", "Mike", "Bob");
    
    Задача:
    
    отсортировать строки по алфавиту

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(5, 10, 15, 20, 25);
-
-        long count =
-                numbers.stream().filter(n -> n > 10).count();
-        System.out.println(count);
+        List<String> names = List.of("Alex", "John", "Mike", "Bob");
+        List<String> result =
+        names.stream().sorted().collect(Collectors.toList());
+        System.out.println(result);
     }
 }

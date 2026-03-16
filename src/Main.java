6f1ad10bfd03e620b425138f2bda3731ed0ac1bb
Jazeas <mssivukhin2@gmail.com>
2026-03-16 20:24:53 +0300

    9. Преобразование строк
    Есть список:
    
    List<String> names = List.of("alex", "john", "mike");
    
    Задача:
    
    сделать все строки в верхнем регистре
    
    Ожидаемый результат:
    
    [ALEX, JOHN, MIKE]

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> words = List.of("Java", "Python", "JS", "Go");
+        List<String> names = List.of("alex", "john", "mike");
 
-        List<String> result = words.stream().filter(n -> n.length() > 3).collect(Collectors.toList());
+        List<String> result = names.stream().map(n -> n.toUpperCase()).collect(Collectors.toList());
         System.out.println(result);
     }
 }

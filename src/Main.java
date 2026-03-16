1aeb6c8b8da370c062f173760a8f7bd6b4d947bb
Jazeas <mssivukhin2@gmail.com>
2026-03-16 19:48:26 +0300

    2. Умножение элементов- Есть список:
    List<Integer> numbers = List.of(1, 2, 3, 4, 5);
    
    Задача:
    
    умножить каждое число на 2
    
    Ожидаемый результат:
    
    [2, 4, 6, 8, 10]

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(3, 7, 10, 15, 20, 25);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
         List<Integer> result =
         numbers.stream()
-                .filter(n -> n > 10)
+                .map(n -> n * 2)
                 .toList();
         System.out.println(result);
 

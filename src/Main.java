343fef4fbe7840d2a3721871ee0dffd78a3d2d2b
Jazeas <mssivukhin2@gmail.com>
2026-03-16 20:18:51 +0300

    7. Длины строк
    Есть список:
    
    List<String> words = List.of("Java", "Spring", "Stream");
    
    Задача:
    
    получить список длин строк
    
    Ожидаемый результат:
    
    [4, 6, 6]

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> names = List.of("Alex", "John", "Mike");
-        List<String> result = names.stream().findFirst().stream().collect(Collectors.toList());
+        List<String> words = List.of("Java", "Spring", "Stream");
+
+        List<Integer> result = words.stream().map(n -> n.length()).collect(Collectors.toList());
         System.out.println(result);
     }
 }

3ecfa1c75241888e49d87c7fbfcee546be3168a8
Jazeas <mssivukhin2@gmail.com>
2026-03-16 20:11:18 +0300

    6. Получить первый элемент
    Есть список:
    
    List<String> names = List.of("Alex", "John", "Mike");
    
    Задача:
    
    получить первый элемент через Stream

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> names = List.of("Alex", "John", "Mike", "Bob");
-        List<String> result =
-        names.stream().sorted().collect(Collectors.toList());
+        List<String> names = List.of("Alex", "John", "Mike");
+        List<String> result = names.stream().findFirst().stream().collect(Collectors.toList());
         System.out.println(result);
     }
 }

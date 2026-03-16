05acb36f186616697e6088f079ac8ee930759b04
Jazeas <mssivukhin2@gmail.com>
2026-03-16 19:54:23 +0300

    3. Удаление дубликатов
    Есть список:
    
    List<Integer> numbers = List.of(1, 2, 2, 3, 3, 3, 4);
    
    Задача:
    
    получить список уникальных чисел
    
    Ожидаемый результат:
    
    [1, 2, 3, 4]

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        List<Integer> numbers = List.of(1, 2, 2, 3, 3, 3, 4);
 
-        List<Integer> result =
-        numbers.stream()
-                .map(n -> n * 2)
-                .toList();
+        List<Integer> result = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
         System.out.println(result);
-
     }
 }

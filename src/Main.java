b79ca4ab6c3d7350e4fb4e19fece9653ea9d063e
Jazeas <mssivukhin2@gmail.com>
2026-03-17 12:17:16 +0300

    Задачи по Exceptions-1. Деление на ноль
    Напиши метод, который принимает два числа и возвращает их деление.
    Если деление на ноль — выведи сообщение "Деление на ноль невозможно" и верни 0.
    
    Вход:
    
    int a = 10;
    int b = 0;
    
    Ожидаемый результат:
    
    Деление на ноль невозможно
    0

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-
+    public static int divide(int a, int b){
+        try {
+            return a / b;
+        }catch (ArithmeticException e){
+            System.out.println("Деление на ноль невозможно");
+            return 0;
+        }
+    }
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+        int a = 10;
+        int b = 0;
 
-        List<Integer> result = numbers.stream().filter(n -> n%2==0).map(n -> n * 10).collect(Collectors.toList());
+        int result = divide(a,b);
         System.out.println(result);
+
     }
 }

7f58e16facfaf23f148b308bda5809e0af7ecb39
Jazeas <mssivukhin2@gmail.com>
2026-03-17 12:24:28 +0300

    3. Конвертация строки в число
    Есть строки:
    
    String s1 = "123";
    String s2 = "abc";
    
    Попробуй преобразовать их в числа с помощью Integer.parseInt.
    Если строка не число — выведи "Неправильный формат: <строка>".
    
    Ожидаемый результат:
    
    123
    Неправильный формат: abc

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,24 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
+        String s1 = "123";
+        String s2 = "abc";
+        int result = 0;
 
         try {
-            System.out.println(arr[5]);
-        } catch (ArrayIndexOutOfBoundsException e ){
-            System.out.println("Такого индекса нет");
+            System.out.println("Работа над строкой " + s1 + " ...");
+            int num1 = Integer.parseInt(s1);
+            System.out.println(num1);
+        } catch (NumberFormatException e ){
+            System.out.println("Неправильный формат: " + s1);
         }
+        try {
+            System.out.println("Работа над строкой " + s2 + " ...");
+            int num2 = Integer.parseInt(s2);
+            System.out.println(num2);
+        } catch (NumberFormatException e ){
+            System.out.println("Неправильный формат: " + s2);
+        }
+
     }
 }
